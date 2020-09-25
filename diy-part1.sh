acee7873dcb8c5b130594105e7742bb32ef901e6
kurtsong <kurt.song@gmail.com>
2020-09-25 15:42:03 +0800

    copy from github.com/Gzxhwq/
    https://github.com/Gzxhwq/OpenWrt-x86_64-firmware

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,14 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+
+#Add external packages
+git submodule add https://github.com/vernesong/OpenClash.git package/external-packages/OpenClash
+git submodule add https://github.com/destan19/OpenAppFilter.git package/external-packages/OpenAppFilter
+
+
